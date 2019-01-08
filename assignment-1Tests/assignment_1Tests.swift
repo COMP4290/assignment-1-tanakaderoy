a7084a209d65c476f82a152c1465516ac27e4d02
tanaka <tanaka@tanakas-iMac.local>
2019-01-08 16:55:13 -0500

    Assignment 1

---
+++ assignment-1Tests/assignment_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  assignment_1Tests.swift
+//  assignment-1Tests
+//
+//  Created by tanaka on 1/8/19.
+//  Copyright © 2019 tanaka. All rights reserved.
+//
+
+import XCTest
+@testable import assignment_1
+
+class assignment_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
